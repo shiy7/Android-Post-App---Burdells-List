4fd67027a84ac8c32d174a34ca11156bd9b86e1c
shane-woodruff <64655640+shane-woodruff@users.noreply.github.com>
2020-11-02 08:47:05 -0500

    Committing updated changes to the shopping Cart

--- app/src/main/java/com/example/finalapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/finalapp/ui/home/HomeFragment.java
@@ -97,8 +97,6 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
     }
 
     private void readPost(Query query){
